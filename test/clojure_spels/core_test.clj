1bf1cfadc39f4dce40358c935dcfb70ebd562f19
Robert Kerr <rskerr@gmail.com>
2013-12-29 10:58:51 -0500

    Initial commit

---
+++ test/clojure_spels/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojure-spels.core-test
+  (:require [clojure.test :refer :all]
+            [clojure-spels.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
